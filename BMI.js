cdbbc2ef9d2b9d0641ffeafadb76d7c6f853162f
Ravindra <ravindrarinwa3@gmail.com>
2024-04-12 19:11:20 +0530

    RR

--- BMI.js
+++ BMI.js
@@ -4,12 +4,12 @@ const weightGain = [
   (trips = {
     name: "Tricps Dips",
     link: "triceps_dips.html",
-    image: "squats_icon.webp",
+    image: "sqauts_icon.webp",
   }),
   (pushUp = {
     name: "Push Up",
     link: "push_up.html",
-    image: "sqauts_img1.jpg",
+    image: "squats_img1.jpg",
   }),
   (squat = {
     name: "Squat",
@@ -17,22 +17,22 @@ const weightGain = [
     image: "sqauts_img3.webp",
   }),
   (RR = {
-    name: "Ravindra Rinwa",
+    name: "Bench Dip",
     link: "ravindra.html",
-    image: "sqauts_img4.webp",
+    image: "BenchDips_1.jpg",
   }),
 ];
 
 const weightLoss = [
   (trips = {
-    name: "Walking",
+    name: "Running",
     link: "triceps_dips.html",
-    image: "squats_icon.webp",
+    image: "Running_icon.webp",
   }),
   (pushUp = {
     name: "Cycling",
     link: "push_up.html",
-    image: "sqauts_img1.jpg",
+    image: "cyclic_icon.webp",
   }),
   (squat = {
     name: "Swiming",
@@ -42,30 +42,30 @@ const weightLoss = [
   (RR = {
     name: "Yoga",
     link: "ravindra.html",
-    image: "sqauts_img4.webp",
+    image: "yoga_icon.jpg",
   }),
 ];
 
 const Healthy = [
   (trips = {
-    name: "RR",
+    name: "Triceps Dips",
     link: "triceps_dips.html",
-    image: "squats_icon.webp",
+    image: "sqauts_img4.webp",
   }),
   (pushUp = {
-    name: "Ak",
+    name: "Push up",
     link: "push_up.html",
-    image: "sqauts_img1.jpg",
+    image: "sqauts_icon.webp",
   }),
   (squat = {
-    name: "Ay",
-    link: "squat.html",
-    image: "sqauts_img3.webp",
+    name: "Swimming",
+    link: "swimming.html",
+    image: "Swimming_icon.jpg",
   }),
   (RR = {
-    name: "AK2",
-    link: "ravindra.html",
-    image: "sqauts_img4.webp",
+    name: "Running",
+    link: "running.html",
+    image: "Running_icon.webp",
   }),
 ];
 
@@ -106,31 +106,33 @@ const process = () => {
   let hv = height.value / 100;
   let wv = weight.value;
 
-  let bmi = wv / (hv * hv);
-  let bmiupdate = bmi.toFixed(2);
-
-  // this is condition used here  to compare all the bmi index
-
-  let addtext = " ";
-  if (bmiupdate > 30) {
-    addtext = " You under the category of obesity";
-
-    output;
-  } else if (bmiupdate > 25 && bmiupdate < 29.9) {
-    addtext = " You under the category of overweight";
-    displayExercise(weightLoss);
-  } else if (bmiupdate > 18.5 && bmiupdate < 24.9) {
-    addtext = " You under the category of Healthy Weight";
-    displayExercise(Healthy);
+  if (hv === "" || wv === "" || hv === 0 || wv === 0) {
+    output.textContent = "Please fill input in right manner";
   } else {
-    addtext = " You under the category of UnderWeight";
-    displayExercise(weightGain);
-    output.textContent = "BMI: " + bmiupdate + ". " + addtext;
+    let bmi = wv / (hv * hv);
+    let bmiupdate = bmi.toFixed(2);
+
+    // this is condition used here  to compare all the bmi index
+
+    let addtext = " ";
+    if (bmiupdate > 30) {
+      addtext = " You under the category of obesity";
+      output;
+    } else if (bmiupdate > 25 && bmiupdate < 29.9) {
+      addtext = " You under the category of overweight";
+      displayExercise(weightLoss);
+    } else if (bmiupdate > 18.5 && bmiupdate < 24.9) {
+      addtext = " You under the category of Healthy Weight";
+      displayExercise(Healthy);
+    } else {
+      addtext = " You under the category of UnderWeight";
+      displayExercise(weightGain);
+      output.textContent = "BMI: " + bmiupdate + ". " + addtext;
+    }
+
+    // this is used to show output
+    output.textContent = "BMI: " + bmi.toFixed(2) + "," + addtext; // Display BMI value with 2 decimal places
   }
-
-  // this is used to show output
-
-  output.textContent = "BMI: " + bmi.toFixed(2) + "," + addtext; // Display BMI value with 2 decimal places
 };
 
 // this is used for the invoke function call here
